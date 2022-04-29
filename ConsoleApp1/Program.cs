a450bd66a26042da60253c13b7909190d312cb11
Hümeysa Aktaş <81477253+Humeysa@users.noreply.github.com>
2022-04-29 14:18:30 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static void metot1(int sayi)
+        {
+            sayi = 1000;
+        }
+
+        private static void metot2(ref int sayi)
+        {
+            sayi = 1000;
+        }
+
+        private static void metot3(out int sayi)
+        {
+            sayi = 1000;
+        }
+
+        static void Main(string[] args)
+        {
+            int deger = 0; //ilk değer ataması yapıldı
+            int deger2; //ilk değer ataması yapılmadı
+
+            // deger değişkeninin kopyası gönderilecektir.
+            // Ekran Çıktısı: metot1() çağrıldıktan sonra:0
+            metot1(deger);
+            Console.WriteLine("metot1() çağrıldıktan sonra:{0}", deger);
+
+            // deger değişkeninin adresi gönderilecektir.
+            // Ekran Çıktısı: metot2() çağrıldıktan sonra:1000
+            metot2(ref deger);
+            Console.WriteLine("metot2() çağrıldıktan sonra:{0}", deger);
+
+            // deger değişkeninin adresi gönderilecektir.
+            // Ekran Çıktısı: metot3() çağrıldıktan sonra:1000
+            metot3(out deger2);
+            Console.WriteLine("metot3() çağrıldıktan sonra:{0}", deger2);
+        }
+    }
+}
